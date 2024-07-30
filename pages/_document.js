de0b56b402b3b3ff0fd43ee94ba44beaf0e01569
aryan-raj-raushan <aryanraj.raushan@gmail.com>
2024-07-30 17:24:55 +0530

    Seo added  for whole page

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,23 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Quantumtech Solutions</title>
+        <meta
+          name="description"
+          content="Quantumtech Solutions provides services like web development, SEO, digital marketing, brand advertising, mobile app development using latest technologies like Next.js, React, Tailwind CSS, Figma, Strapi, Node.js, MongoDB, Firebase, MERN stack, Full Stack, WordPress, React Native, Shopify, Android, Ios, Mobile optimization, Responsive Design"
+        />
+        <meta
+          name="keywords"
+          content="web development, SEO, digital marketing, brand advertising, mobile app, Next.js, React, Tailwind CSS, Figma, Strapi, Node.js, MongoDB, Firebase, MERN stack, Full Stack, WordPress, React Native, Shopify, Android, Ios, Mobile optimization, Responsive Design, Payment Gateway, Razorpay, stripe, Billdesk, paytm integration "
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="Quantumtech Solutions" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
