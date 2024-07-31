4e1c6623d2b2811a2150c261c10bd7006a2b4706
saurabhp0007 <saurabhpanday21212@gmail.com>
2024-07-31 19:47:32 +0530

    fix

--- components/common/Card.js
+++ components/common/Card.js
@@ -2,21 +2,25 @@ import Link from "next/link";
 import { TitleSm } from "./Title";
 import { HiOutlineArrowRight } from "react-icons/hi";
 
-export const Card = ({ data, caption, show, path = "#" ,height}) => {
+export const Card = ({ data, caption, show, path = "#", height }) => {
   return (
     <>
       <div className="card">
         <div className="card-img">
-          <img src={data.cover} alt={data.title} className={`w-96 ${height ? `h-${height}` : 'h-full'}`}  />
+          <img 
+            src={data.cover} 
+            alt={data.title} 
+            className={`w-96 ${height || 'h-full'}`}  
+          />
         </div>
         <div className="card-details">
-          <Link href={path} legacyBehavior>
+          <Link href={path} passHref legacyBehavior>
             <a target="_blank" rel="noopener noreferrer" className="title-link">
               <TitleSm title={data.title} />
             </a>
           </Link>
           {caption && (
-            <Link href={`${path}/${data.id}`} legacyBehavior>
+            <Link href={`${path}/${data.id}`} passHref legacyBehavior>
               <a target="_blank" rel="noopener noreferrer">
                 {caption} <HiOutlineArrowRight className="link-icon" />
               </a>
--- sections/Team.js
+++ sections/Team.js
@@ -19,7 +19,7 @@ const Team = () => {
           </div>
           <div className="grid-4 py lg:max-w-screen-xl lg:mx-auto">
             {teamdata.map((item) => (
-              <Card data={item} key={item.id} caption={item.post} height={"96"} />
+              <Card data={item} key={item.id} caption={item.post} height="h-96" />
             ))}
           </div>
         </div>
